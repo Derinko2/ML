4162eef897700662abdf4e30d5ef8c41166ee1f0
Derinko2 <37025219+Derinko2@users.noreply.github.com>
2020-06-28 14:08:44 -0400

    Add files via upload

---
+++ flip.js
@@ -0,0 +1,8 @@
+var flip =getElementById('flip') 
+function hover(flip) {
+  flip.setAttribute('src', 'http://dummyimage.com/100x100/eb00eb/fff');
+}
+
+function unhover(flip) {
+  flip.setAttribute('src', 'http://dummyimage.com/100x100/000/fff');
+}
